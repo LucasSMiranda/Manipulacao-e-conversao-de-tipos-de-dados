9532542d4fee0c38d3030abb7022eb3789e2492f
Lucas Miranda <104878497+LucasSMiranda@users.noreply.github.com>
2026-03-23 23:48:05 +0000

    divisão de números inteiros

---
+++ manip_conv_tip_dados.C
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+//Manipulção de números inteiros
+/*int main() {
+    int a = 10;
+    int b = 3;
+    int soma = a + b;
+    int diferenca = a - b;
+    int produto = a * b;
+    int quociente = a / b; // Note que a divisão de inteiros resulta em um número inteiro
+ 
+    printf("Soma: %d\n", soma);
+    printf("Diferença: %d\n", diferenca);
+    printf("Produto: %d\n", produto);
+    printf("Quociente: %d\n", quociente);*/
+
+    #include <stdio.h>
+ 
+
+    //Divisão de números inteiros
+    /*
+int main() {
+    int a = 7;
+    int b = 2;
+    int quociente = a / b; // Note que a divisão de 7 por 2 resulta em 3, pois a parte decimal é descartada
+ 
+    printf("Quociente: %d\n", quociente);
+ 
+    return 0;
+}
+    */
+ 
+    return 0;
+}
