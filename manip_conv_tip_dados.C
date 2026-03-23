a55dd5d174a07321e63cfcce241c460c104a34fd
Lucas Miranda <104878497+LucasSMiranda@users.noreply.github.com>
2026-03-23 23:52:24 +0000

    conversão explícita

--- manip_conv_tip_dados.C
+++ manip_conv_tip_dados.C
@@ -70,5 +70,21 @@ int main() {
 }*/
 
 
+//Conversão explícita:
+/*
+
+#include <stdio.h>
+ 
+int main() {
+    int a = 10;
+    int b = 3;
+    float quociente = (float) a / b; // 'a' é explicitamente convertido para float
+ 
+    printf("Quociente: %.2f\n", quociente);
+ 
+    return 0;
+}
+    */ 
+
    
 }
